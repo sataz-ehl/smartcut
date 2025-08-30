4d0cf3ce6bf837d3163be46cba41ebb8d33984ec
Santtu Keskinen <santtu.keskinen@gmail.com>
2025-08-30 15:12:09 +0300

    Tweak linux filename

--- distribution/pyinstaller/linux_package.sh
+++ distribution/pyinstaller/linux_package.sh
@@ -8,4 +8,5 @@ rm -rf build dist
 pyinstaller --distpath ./dist --workpath ./build --onefile -n smartcut smartcut/__main__.py
 
 tar -czvf "dist/smartcut_linux.tar" -C dist smartcut
-./dist/smartcut --version
+mv ./dist/smartcut dist/smartcut_x86_64
+./dist/smartcut_x86_64 --version
